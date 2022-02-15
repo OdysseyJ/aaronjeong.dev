a16fa0cb701ef8f66a90b6d5d94e1e6289dab543
Odysseyj <odysseyj@naver.com>
2022-02-16 01:13:18 +0900

    코멘트 변경

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,18 +1,20 @@
-import { useColorMode } from "@chakra-ui/react";
-import { DiscussionEmbed } from "disqus-react";
+import {useEffect} from "react";
 
 type CommentsProps = {
   config: any
 }
 
 const Comments: React.FC<CommentsProps> = ({config}) => {
-  const { colorMode } = useColorMode();
+  useEffect(()=>{
+    (function() { // DON'T EDIT BELOW THIS LINE
+      var d = document, s = d.createElement('script');
+      s.src = 'https://aaronjeong.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', new Date().toISOString());
+      (d.head || d.body).appendChild(s);
+    })();
+  })
 
-  return (
-    <DiscussionEmbed
-        shortname="aaronjeong"
-        config={config}
-    />
-  );
+  return (<div id="disqus_thread"></div>)
 };
+
 export default Comments;
