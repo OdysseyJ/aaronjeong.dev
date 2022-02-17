04dfc8f2286cf0d86cc40da9f24426791d0a5e15
Odysseyj <odysseyj@naver.com>
2022-02-17 19:20:53 +0900

    2/17-firebase-fcm

--- playground.py
+++ playground.py
@@ -3,22 +3,28 @@ sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
 
-def solve(N, start, to, via):
-    if N == 1:
-        print(f"{start} {to}")
+def post_order(start, end):
+    if start > end:
         return
-    solve(N-1, start, via, to)
-    solve(1, start, to, via)
-    solve(N-1, via, to, start)
-    return
+
+    root = pre_order[start]
+    idx = start + 1
+
+    while idx <= end:
+        if pre_order[idx] > root:
+            break
+        idx += 1
+
+    post_order(start+1, idx-1)
+    post_order(idx, end)
+    print(root)
 
 
-n = int(input())
-dp = [0 for _ in range(n+1)]
-dp[0] = 0
-dp[1] = 1
-for i in range(2, n+1):
-    dp[i] = 2*dp[i-1]+1
-print(dp[i])
-if n <= 20:
-    solve(n, '1', '3', '2')
+pre_order = []
+while True:
+    try:
+        pre_order.append(int(input()))
+    except:
+        break
+
+post_order(0, len(pre_order) - 1)
