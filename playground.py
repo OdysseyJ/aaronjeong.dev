e1af15df72cf650e5e564682ec4b170fc376556a
Odysseyj <odysseyj@naver.com>
2022-02-19 15:54:55 +0900

    2/19-ps

--- playground.py
+++ playground.py
@@ -3,28 +3,38 @@ sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
 
-def post_order(start, end):
-    if start > end:
+class Node(object):
+    left = None
+    right = None
+    value = None
+
+    def __init__(self, **kwargs):
+        self.value = kwargs.get("value", None)
+
+
+def pre_order(in_start, in_end, post_start, post_end):
+    if(in_start > in_end) or (post_start > post_end):
         return
 
-    root = pre_order[start]
-    idx = start + 1
+    root = post_order[post_end]
+    print(root, end=" ")
 
-    while idx <= end:
-        if pre_order[idx] > root:
-            break
-        idx += 1
+    left = positions[root] - in_start
+    right = in_end - positions[root]
 
-    post_order(start+1, idx-1)
-    post_order(idx, end)
-    print(root)
+    # left
+    pre_order(in_start, in_start+left-1, post_start, post_start+left-1)
+    # right
+    pre_order(in_end-right+1, in_end, post_end-right, post_end-1)
 
 
-pre_order = []
-while True:
-    try:
-        pre_order.append(int(input()))
-    except:
-        break
+k = int(input())
+in_order = [int(i) for i in input().split(" ")]
+post_order = [int(i) for i in input().split(" ")]
 
-post_order(0, len(pre_order) - 1)
+positions = [0] * (k+1)
+for i in range(k):
+    positions[in_order[i]] = i
+
+pre_order(0, k-1, 0, k-1)
+print()
