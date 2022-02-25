e6f6aee4d4a237533c7ded28794cb1a63f6a54a1
Odysseyj <odysseyj@naver.com>
2022-02-25 20:45:28 +0900

    2/25-ps

--- playground.py
+++ playground.py
@@ -1,33 +1,12 @@
 import sys
-sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+n = int(input().strip())
+nums = map(int, input().strip().split())
+sorted_nums = sorted(nums)
+dp = [0 for _ in range(n)]
+dp[0] = sorted_nums[0]
 
+for idx, num in enumerate(sorted_nums[1:]):
+  dp[idx+1] = dp[idx] + num
 
-def is_pelindrom(s):
-    return True if s == s[::-1] else False
-
-
-def solve(ss):
-    global akaraka_pelindrom
-    if len(ss) == 1:
-        return ss
-
-    half = int(len(ss)/2)
-    if len(ss) % 2 == 0:
-        b_half = ss[:half]
-        a_half = ss[half:]
-    else:
-        b_half = ss[:half]
-        a_half = ss[half+1:]
-    if b_half != a_half:
-        akaraka_pelindrom = False
-    elif not is_pelindrom(ss):
-        akaraka_pelindrom = False
-    elif not is_pelindrom(solve(b_half)):
-        akaraka_pelindrom = False
-    return ss
-
-
-s = input()
-akaraka_pelindrom = True
-solve(s)
-print("AKARAKA" if akaraka_pelindrom else "IPSELENTI")
+print(sum(dp))
