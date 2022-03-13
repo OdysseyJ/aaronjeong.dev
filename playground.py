3db6bc6a0e78548bc3200cae3424b3e329e76da4
Odysseyj <odysseyj@naver.com>
2022-03-13 19:09:05 +0900

    3/13-ps

--- playground.py
+++ playground.py
@@ -1,27 +1,8 @@
 import sys
-sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
+n = int(input())
+dots = sorted([int(i) for i in [input().split() for _ in range(n)]], key=lambda d: (d[0], d[1]))
 
-def back_tracking(arr):
-    if len(arr) == m:
-        if m == 1 and arr[0] not in duplicated:
-            duplicated[arr[0]] = True
-            print(arr[0])
-        elif " ".join(arr) not in duplicated:
-            result = " ".join(arr)
-            duplicated[result] = True
-            print(result)
-        return
-
-    for idx, num in enumerate(nums):
-        if len(arr) == 0 or int(arr[-1]) <= num:
-            arr.append(str(num))
-            back_tracking(arr)
-            arr.pop()
-
-
-n, m = map(int, input().split())
-nums = sorted([int(i) for i in input().split()])
-duplicated = {}
-back_tracking([])
+for (x, y) in dots:
+    print(f"{x} {y}")
