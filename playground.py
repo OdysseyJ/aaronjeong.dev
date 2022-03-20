cef89d2ae93680b5453f0ad4aac90b290855a9cd
Odysseyj <odysseyj@naver.com>
2022-03-20 16:55:01 +0900

    3/20-rds

--- playground.py
+++ playground.py
@@ -3,14 +3,10 @@ input = sys.stdin.readline
 
 n = int(input())
 for i in range(1, n+1):
-    for j in range(n-i):
-        print(" ", end="")
     for j in range(i):
         print("*", end="")
     print()
 for i in range(1, n):
-    for j in range(i):
-        print(" ", end="")
     for j in range(n-i):
         print("*", end="")
     print()
