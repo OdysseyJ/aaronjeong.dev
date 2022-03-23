b532449d44a09c8f3cfef6cc3773c68bc8f0fb21
Odysseyj <odysseyj@naver.com>
2022-03-23 20:34:57 +0900

    3/23-aws

--- playground.py
+++ playground.py
@@ -2,11 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-for i in range(1, n+1):
-    for j in range(i):
+for i in range(n):
+    if i == n-1:
+        for j in range(2*i+1):
+            print("*", end="")
+    else:
+        for j in range(n-i-1):
+            print(" ", end="")
         print("*", end="")
-    print()
-for i in range(1, n):
-    for j in range(n-i):
-        print("*", end="")
+        for j in range(2*i-1):
+            print(" ", end="")
+        if 2*i-1 > 0:
+            print("*", end="")
     print()
