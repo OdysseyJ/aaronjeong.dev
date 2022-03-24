3030120970124a944dc090e9b259ae4b7bb4e2b3
Odysseyj <odysseyj@naver.com>
2022-03-25 00:37:23 +0900

    whatisthetime

--- playground.py
+++ playground.py
@@ -3,15 +3,9 @@ input = sys.stdin.readline
 
 n = int(input())
 for i in range(n):
-    if i == n-1:
-        for j in range(2*i+1):
-            print("*", end="")
-    else:
-        for j in range(n-i-1):
-            print(" ", end="")
-        print("*", end="")
-        for j in range(2*i-1):
-            print(" ", end="")
-        if 2*i-1 > 0:
-            print("*", end="")
+    for j in range(n):
+        if i%2 == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end="*")
     print()
