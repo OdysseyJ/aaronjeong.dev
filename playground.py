eedf3128166b1b6e9b20fc632ddf5b1c86202e29
Odysseyj <odysseyj@naver.com>
2022-03-27 22:38:31 +0900

    3/27

--- playground.py
+++ playground.py
@@ -2,25 +2,26 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-printer = [[" " for _ in range(4*n-3)] for _ in range(4*n-1)]
+printer = [[" " for _ in range(4*n-3)] for _ in range(2*n-1)]
+gap = n-2
 
-if n == 1:
-    print("*")
-else:
-    for i in range(n):
-        start = 2*i
-        width = 4*(n-i)-3
-        height = 4*(n-i)-1
-        for j in range(start, start+width):
-            printer[start][j] = "*"
-        for j in range(start, start+width):
-            printer[start+height-1][j] = "*"
-        for j in range(start, start+height):
-            printer[j][start] = "*"
-        for j in range(start+2, start+height):
-            printer[j][start+width-1] = "*"
-        if i != 0:
-            printer[start][start+width] = "*"
+for i in range(n):
+    printer[0][i] = "*"
+    printer[0][i+n+(2*n-3)] = "*"
+    printer[(2*n-1)-1][i] = "*"
+    printer[(2*n-1)-1][i+n+(2*n-3)] = "*"
 
-    for idx, line in enumerate(printer):
-        print("".join(line).strip())
+for i in range(n):
+    printer[i][i] = "*"
+    printer[i][i+n-1] = "*"
+    printer[i][(4*n-3)-i-n] = "*"
+    printer[i][(4*n-3)-i-1] = "*"
+
+for i in range(n):
+    printer[(2*n-1)-i-1][i] = "*"
+    printer[(2*n-1)-i-1][i+n-1] = "*"
+    printer[(2*n-1)-i-1][n+(2*n-3)-i] = "*"
+    printer[(2*n-1)-i-1][n+n+(2*n-3)-i-1] = "*"
+
+for line in printer:
+    print("".join(line).rstrip())
