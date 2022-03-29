9c31439b46ea6d70c630a33c7860be032dc60bca
Odysseyj <odysseyj@naver.com>
2022-03-29 11:54:47 +0900

    3/29-aws

--- playground.py
+++ playground.py
@@ -1,27 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-printer = [[" " for _ in range(4*n-3)] for _ in range(2*n-1)]
-gap = n-2
-
-for i in range(n):
-    printer[0][i] = "*"
-    printer[0][i+n+(2*n-3)] = "*"
-    printer[(2*n-1)-1][i] = "*"
-    printer[(2*n-1)-1][i+n+(2*n-3)] = "*"
-
-for i in range(n):
-    printer[i][i] = "*"
-    printer[i][i+n-1] = "*"
-    printer[i][(4*n-3)-i-n] = "*"
-    printer[i][(4*n-3)-i-1] = "*"
-
-for i in range(n):
-    printer[(2*n-1)-i-1][i] = "*"
-    printer[(2*n-1)-i-1][i+n-1] = "*"
-    printer[(2*n-1)-i-1][n+(2*n-3)-i] = "*"
-    printer[(2*n-1)-i-1][n+n+(2*n-3)-i-1] = "*"
-
-for line in printer:
-    print("".join(line).rstrip())
+while True:
+    try:
+        a, b = map(int, input().split())
+        print(a + b)
+    except:
+        break
