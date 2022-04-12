63ca6ea75204d7441199b358e4c8953cbe7783c4
Odysseyj <odysseyj@naver.com>
2022-04-12 22:29:58 +0900

    4/12

--- playground.py
+++ playground.py
@@ -1,16 +1,8 @@
 import sys
 input = sys.stdin.readline
 
-odds = []
-while True:
-    try:
-        num = int(input())
-        if num%2 != 0:
-            odds.append(num)
-    except:
-        break
-if not odds:
-    print(-1)
-else:
-    print(sum(odds))
-    print(sorted(odds)[0])
+n = int(input())
+for _ in range(n):
+    count = int(input())
+    nums = map(int, input().split())
+    print(sum(nums))
