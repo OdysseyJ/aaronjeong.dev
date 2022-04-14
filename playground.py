4dfd7726a46c3c46a3e7cc24972768851d222466
Odysseyj <odysseyj@naver.com>
2022-04-14 19:15:51 +0900

    4/14

--- playground.py
+++ playground.py
@@ -3,6 +3,9 @@ input = sys.stdin.readline
 
 n = int(input())
 for _ in range(n):
-    count = int(input())
-    nums = map(int, input().split())
-    print(sum(nums))
+    a, b = map(int, input().split())
+    print(2-a+b)
+
+# 2
+# 8 12
+# 4 6
