975f5b76d91fd326bab7bce5d9f703dffdc1e6fa
Odysseyj <odysseyj@naver.com>
2022-04-17 16:13:35 +0900

    4/17

--- playground.py
+++ playground.py
@@ -2,10 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-for _ in range(n):
-    a, b = map(int, input().split())
-    print(2-a+b)
+nums = map(int, input().split())
+dp = [0] * n
 
-# 2
-# 8 12
-# 4 6
+for idx, num in enumerate(nums):
+    if num == 1:
+        if idx == 0:
+            dp[idx] = 1
+        else:
+            dp[idx] = dp[idx-1] + 1
+print(sum(dp))
