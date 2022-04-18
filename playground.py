431d07a937b719c2d8b68517ed9f5f76f7b30637
Odysseyj <odysseyj@naver.com>
2022-04-18 19:45:06 +0900

    4/18

--- playground.py
+++ playground.py
@@ -1,14 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-nums = map(int, input().split())
-dp = [0] * n
+print("a" "b")
+print("asdffasdf".find("asd"))
+print("asdffasdf".index("asd"))
 
-for idx, num in enumerate(nums):
-    if num == 1:
-        if idx == 0:
-            dp[idx] = 1
-        else:
-            dp[idx] = dp[idx-1] + 1
-print(sum(dp))
+abc = "Hello World"
+print(abc[0:4])
+
+while True:
+    print("asbt")
