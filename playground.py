76d887ea69af1da3ad5875dbf8678daaff425bca
Odysseyj <odysseyj@naver.com>
2022-04-19 21:11:22 +0900

    4/19

--- playground.py
+++ playground.py
@@ -1,12 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-print("a" "b")
-print("asdffasdf".find("asd"))
-print("asdffasdf".index("asd"))
+primes = [True for _ in range(10001)]
+primes[1] = False
 
-abc = "Hello World"
-print(abc[0:4])
+for i, is_prime in enumerate(primes):
+    if i in [0, 1]:
+        continue
+    if is_prime:
+        for j in range(i*i, 10001, i):
+            primes[j] = False
 
-while True:
-    print("asbt")
+m = int(input())
+n = int(input())
+
+total = 0
+min_num = 0
+for i in range(m, n+1):
+    if primes[i]:
+        total += i
+        if not min_num:
+            min_num = i
+
+if total:
+    print(total)
+    print(min_num)
+else:
+    print(-1)
