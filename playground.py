1db75782b392aa4df607c477b676885b94a3e03d
Odysseyj <odysseyj@naver.com>
2022-04-21 14:41:20 +0900

    4/21 - sql

--- playground.py
+++ playground.py
@@ -1,29 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-primes = [True for _ in range(10001)]
-primes[1] = False
-
-for i, is_prime in enumerate(primes):
-    if i in [0, 1]:
-        continue
-    if is_prime:
-        for j in range(i*i, 10001, i):
-            primes[j] = False
-
-m = int(input())
 n = int(input())
 
 total = 0
-min_num = 0
-for i in range(m, n+1):
-    if primes[i]:
-        total += i
-        if not min_num:
-            min_num = i
+for i in range(n+1):
+    total += int((3*i*(i+1))/2)
 
-if total:
-    print(total)
-    print(min_num)
-else:
-    print(-1)
+print(total)
