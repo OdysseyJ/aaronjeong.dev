19a3b1dc674ed123797c3176ad9340dbe4bf853e
Odysseyj <odysseyj@naver.com>
2022-04-23 19:47:01 +0900

    4/23-ps

--- playground.py
+++ playground.py
@@ -4,5 +4,10 @@ input = sys.stdin.readline
 n = int(input())
 
 for i in range(n):
-    a, b = map(int, input().split())
-    print(f"You get {a // b} piece(s) and your dad gets {a % b} piece(s).")
+    car_price = int(input())
+    options_cnt = int(input())
+    total_price = car_price
+    for j in range(options_cnt):
+        cnt, price = map(int, input().split())
+        total_price += (cnt * price)
+    print(total_price)
