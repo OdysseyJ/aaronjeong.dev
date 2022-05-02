350eca19582b5f12d0ba0492e945280bf4c0f71e
Odysseyj <odysseyj@naver.com>
2022-05-02 12:05:40 +0900

    5/2

--- playground.py
+++ playground.py
@@ -1,11 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-idx = 0
-total = 0
-for i in range(1, 6):
-    nums = map(int, input().split())
-    if (current:= sum(nums)) > total:
-        idx = i
-        total = current
-print(idx, total)
+n = int(input())
+for i in range(n):
+    num = int(input())
+    idx = 0
+    while True:
+        if num == 0:
+            break
+        if num % 2 == 1:
+            print(idx, end=" ")
+        num = int(num / 2)
+        idx += 1
+    print()
+
+# 1
+# 13
