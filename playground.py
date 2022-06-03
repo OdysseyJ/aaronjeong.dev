1147f41498cfc08d4bc593fd4a13644865f4b2f8
Odysseyj <odysseyj@naver.com>
2022-06-03 17:35:33 +0900

    6/3-designpattern

--- playground.py
+++ playground.py
@@ -1,30 +1,81 @@
-def inclination(p1, p2):
-    return p2[0] - p1[0], p2[1] - p1[1]
+from abc import *
 
-def ccw(p1, p2, p3):
-    v, u = inclination(p1, p2), inclination(p2, p3)
-    return v[0] * u[1] - v[1] * u[0]
 
-def convex_hull(positions):
-    convex = list()
-    for p3 in positions:
-        while len(convex) >= 2:
-            p1, p2 = convex[-2], convex[-1]
-            print(p1, p2, p3, ccw(p1, p2, p3) > 0)
-            if ccw(p1, p2, p3) > 0:
-                break
-            convex.pop()
-        convex.append(p3)
-    return len(convex)
+class FlyBehavior(metaclass=ABCMeta):
+    def fly(self):
+        pass
 
-n, answer = int(input()), -2
-positions = list()
-for i in range(n):
-    positions.append(list(map(int, input().split())))
 
-positions = sorted(positions, key=lambda pos:(pos[1], pos[0]))
-answer += convex_hull(positions)
+class FlyWithWing(FlyBehavior):
+    def fly(self):
+        print("날개로 날기")
 
-positions.reverse()
-answer += convex_hull(positions)
-print(answer)
+
+class FlyWithRocket(FlyBehavior):
+    def fly(self):
+        print("로켓으로 날기")
+
+
+class QuackBehavior(metaclass=ABCMeta):
+    def quack(self):
+        pass
+
+
+class Quack(QuackBehavior):
+    def quack(self):
+        print("그냥꽥꽥")
+
+
+class SlowQuack(QuackBehavior):
+    def quack(self):
+        print("꽤액")
+
+
+class Duck(metaclass=ABCMeta):
+    fly_behavior: FlyBehavior
+    quack_behavior: QuackBehavior
+
+    def perform_quack(self):
+        return self.quack_behavior.quack()
+
+    def perform_fly(self):
+        return self.fly_behavior.fly()
+
+    def swim(self):
+        print("헤엄치기")
+
+    def quack(self):
+        print("꽥꽥")
+
+    def set_fly_behavior(self, fly_behavior: FlyBehavior):
+        self.fly_behavior = fly_behavior
+
+    def set_quack_behavior(self, quack_behavior: QuackBehavior):
+        self.quack_behavior = quack_behavior
+
+    @abstractmethod
+    def display(self):
+        raise NotImplementedError()
+
+
+class YellowDuck(Duck):
+    def __init__(self):
+        self.fly_behavior = FlyWithWing()
+        self.quack_behavior = Quack()
+
+    def display(self):
+        print("노란오리")
+
+
+class RubberDuck(Duck):
+    def quack(self):
+        print("삑삑")
+
+    def display(self):
+        print("고무오리")
+
+
+yellow_duck = YellowDuck()
+yellow_duck.perform_fly()
+yellow_duck.set_fly_behavior(fly_behavior=FlyWithRocket())
+yellow_duck.perform_fly()
