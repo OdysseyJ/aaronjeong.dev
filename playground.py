aff833e529c3ff7a54ad407ee177e6b884bbe53a
Odysseyj <odysseyj@naver.com>
2022-06-11 22:39:29 +0900

    6/11

--- playground.py
+++ playground.py
@@ -7,32 +7,3 @@ range_find = text.find("h", 0, 1)
 print(range_find)
 print(f_result)
 print(i_result)
-
-# Find와 Index는 없을때 차이가 난다.
-# ff_result = text.find("z")
-# ii_result = text.index("z")
-# print(ff_result)
-# print(ii_result)
-
-# Count
-c_result = text.count(" ")
-print(c_result)
-
-# Startswith
-s_result = text.startswith("h")
-ss_result = text.startswith("j")
-S_result = text.startswith("H")
-SS_result = text.upper().startswith("H")
-print(s_result)
-print(ss_result)
-print(S_result)
-print(SS_result)
-
-test = "hello world"
-print("#####")
-print(test.capitalize())
-print(test.upper())
-print("#####")
-print(len("assdf"))
-
-print(sorted([10, 5, 20], reverse=True))
