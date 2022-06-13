c21fe356cd98866350739ecd3419ff618a57a2e0
Odysseyj <odysseyj@naver.com>
2022-06-13 21:59:14 +0900

    remove playgroud

--- playground.py
+++ playground.py
@@ -1,9 +1,1 @@
-text = "hello world jane aaron"
-
-# 기본적으로 있을때는 똑같이 동작한다.
-f_result = text.find("jane")
-i_result = text.index("jane")
-range_find = text.find("h", 0, 1)
-print(range_find)
-print(f_result)
-print(i_result)
+asdf
