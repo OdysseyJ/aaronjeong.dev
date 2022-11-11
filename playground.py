68d1a96d8ae5bca5dd5448f35f4c02017ade8466
seongwoon jeong <ggogumalove@gmail.com>
2022-11-11 16:56:59 +0900

    11/11

--- playground.py
+++ playground.py
@@ -1,25 +1,46 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
-queue = deque([])
+line = input().strip()
 
-new_queue = []
+stack = []
+result = 0
+prev = None
+for char in line:
+    if char == "(":
+        stack.append(char)
+    elif char == ")":
+        stack.pop()
+        if prev == ")":
+            result += 1
+        else:
+            result += len(stack)
+    prev = char
+print(result)
 
+import sys
 
-def push(k: int):
-    queue.append(k)
-    new_queue.append(k)
-
-
-def pop():
-    return queue.popleft()
-
-
-def front():
-    return queue[0]
-
-
-def back():
-    return queue[-1]
+# input = sys.stdin.readline
+#
+# line = input().strip()
+#
+# stack = []
+# result = 0
+# for char in line:
+#     if char == ")":
+#         num = 0
+#         while top := stack.pop() != "(":
+#             num += top
+#         num = 2 * num if num else 2
+#         stack.append(num)
+#     elif char == "]":
+#         num = 0
+#         while top := stack.pop() != "[":
+#             num += top
+#         num = 3 * num if num else 3
+#         stack.append(num)
+#     else:
+#         stack.append(char)
+#
+#
